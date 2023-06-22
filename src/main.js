02305470542b7420626dc8853fd6902c428ddb2b
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-06-22 14:58:09 +0200

    improve style

--- src/main.js
+++ src/main.js
@@ -1,39 +1,42 @@
 // Toggle hamburger menu
 $('#hamburger-icon').on('click', () => {
-  $('#nav-container').toggle()
+  $('#nav-container').slideToggle('fast', function () {
+    $('.hamburger-menu').toggleClass(
+      'expanded',
+      $('#nav-container').is(':visible')
+    )
+    $('#hamburger-icon').attr(
+      'src',
+      $('#nav-container').is(':visible')
+        ? '../images/close.svg'
+        : '../images/hamburger.svg'
+    )
+  })
   $('#main').toggle()
+})
 
-  if ($('#nav-container').is(':visible')) {
-    $('.hamburger-menu').addClass('expanded')
-    $('#hamburger-icon').attr('src', '../images/close.svg')
-  } else {
-    $('.hamburger-menu').removeClass('expanded')
-    $('#hamburger-icon').attr('src', '../images/hamburger.svg')
-  }
+// Hamburger menu > over-ons accordion toggle
+$('.accordion-header').on('click', function () {
+  var accordionContent = $(this).next('.accordion-content')
+  var accordionIcon = $(this).find('.accordion-icon')
+
+  accordionContent.slideToggle('fast')
+  accordionIcon.toggleClass('active')
 })
 
 // Toggle search box
 
 $('#search-button').on('click', () => {
-  $('#search-box').toggle()
+  $('#search-box').toggleClass('open')
 })
 
 $('#hamburger-search-button').on('click', () => {
-  $('#search-box').toggle()
+  $('#search-box').toggleClass('open')
 })
-
-// $('#slideContent').html(`
-// <h1>${slideContent[0].title.replace(/\n/g, '<br>')}</h1>
-// <h2>${slideContent[0].secondaryTitle}</h2>
-// <p>${slideContent[0].secondaryTitle}</p>
-// `)
 
 // Carousel
 const track = $('#carousel')
 const slides = Array.from(track.children())
-
-// const slideWidth = slides[0].getBoundingClientRect().width
-// console.log(slideWidth, window.innerWidth)
 
 // Set slide position
 slides.forEach((slide, index) => {
@@ -50,6 +53,8 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
 $('.carousel__slide__container__footer .next-button').on('click', () => {
   const currentSlide = $('#carousel .current-slide')
   const nextSlide = currentSlide.next()
+
+  console.log(currentSlide)
 
   if (nextSlide.length === 0) {
     // No next slide, disable the next button
